2717e30d5164569e9a75a583d94d6b80d7f30256
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-11-17 15:16:09 +0600

    Banner section created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Banner from './components/Banner';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Banner />
       <h1 className="text-center text-3xl">This is home page</h1>
     </>
   );
