46c0fdb12a1c218968e5a5aa3ad5adf52473114b
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-11-17 14:35:33 +0600

    navbar created

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1 className="text-center text-3xl">This is home page</h1>
+    </>
+  );
+}
+
+export default App;
